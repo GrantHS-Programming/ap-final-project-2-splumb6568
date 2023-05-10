bb722a791f0f5adc6393576d6d49b301bd9cc022
splumb6568 <splumb6568@student.pps.net>
2023-05-10 13:48:54 -0700

    third commit

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerMoment : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
