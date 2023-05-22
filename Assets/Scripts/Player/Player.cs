abd3eca5530148d1c51bf07845a239806a0562be
splumb6568 <splumb6568@student.pps.net>
2023-05-22 13:51:31 -0700

    eight commit

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,12 +8,30 @@ public class Player : MonoBehaviour
     int hp;
     public float bulletCooldown;
     float bulletTimer;
+
+    public GameObject[] hearts;
+    public int life;
+
     void Start()
     {
         hp = startHp;
     }
     void Update()
     {
+
+        if (hp < 1)
+        {
+            Destroy(hearts[0].gameObject);
+        }
+        else if (hp < 2)
+        {
+            Destroy(hearts[1].gameObject);
+        }
+        else if (hp < 3)
+        {
+            Destroy(hearts[2].gameObject);
+        }
+
         bulletTimer -= Time.deltaTime;
     }
     private void OnTriggerEnter2D(Collider2D collision)
